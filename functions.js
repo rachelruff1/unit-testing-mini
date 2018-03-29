dbcf7f9e2981f0847582457d93d3350644aa704f
Rachel Ruff <rachelruff@Rachels-MacBook-Pro.local>
2018-03-29 11:44:22 -0500

    finished

--- functions.js
+++ functions.js
@@ -2,10 +2,19 @@ const returnTwo = () => 2;
 
 const greeting = name => `Hello, ${name}`;
 
-const add = (num1, num2) => Math.floor(num1) + Math.floor(num2);
+const add = (num1, num2) => {
+  return Math.round(num1) + Math.round(num2);
+};
+
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+const subtract = (num1, num2) => num1 - num2;
 
 module.exports = {
-    returnTwo,
-    greeting,
-    add
-}
+  returnTwo,
+  greeting,
+  add,
+  multiply,
+  divide,
+  subtract
+};
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const { returnTwo, greeting, add } = require('./functions');
+const { returnTwo, greeting, add, multiply, divide, subtract } = require('./functions');
 
 test('returnTwo should return the number 2', () => {
     expect(returnTwo()).toEqual(2);
@@ -14,3 +14,25 @@ test("add() should return the sum of 2 passed in integers", () => {
     expect(add(3, 5)).toEqual(8);
     expect(add(0.1, 0.2)).toEqual(0);
 })
+
+describe("Math functions:", () => {
+    test("add() should return a dynamic sum based on two number parameters.", () => {
+      expect( add( 1, 2 ) ).toEqual( 3 );
+      expect( add( 5, 9 ) ).toEqual( 14 );
+    });
+  
+    test("multiply() should return a dynamic product based on two number parameters.", () => {
+      expect( multiply( 1, 2 ) ).toEqual( 2 );
+      expect( multiply( 5, 9 ) ).toEqual( 45 );
+    });
+  
+    test("divide() should return a dynamic quotient based on two number parameters.", () => {
+      expect( divide( 2, 1 ) ).toEqual( 2 );
+      expect( divide( 9, 3 ) ).toEqual( 3 );
+    });
+  
+    test("subtract() should return a dynamic difference based on two number parameters.", () => {
+      expect( subtract( 2, 1 ) ).toEqual( 1 );
+      expect( subtract( 9, 3 ) ).toEqual( 6 );
+    });
+  });
